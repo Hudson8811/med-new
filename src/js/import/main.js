c046a506ccaa63d93f16fa57eb0fb3db3072b62b
unknown <dmitryboz21@yandex.com>
2024-02-02 17:14:45 +0300

    123

--- src/js/import/main.js
+++ src/js/import/main.js
@@ -1,5 +1,65 @@
-jQuery(function () { 
+jQuery(function ($) {
+	function sel2WithIconsTheme(state) {
+		if (!state.id) {
+			return state.text;
+		}
+		var imgSrc = $(state.element).attr("data-img-src");
+		if (typeof imgSrc !== "undefined" && imgSrc.length>0) {
+			var $state = $(
+				'<span><img src="' +
+					$(state.element).attr("data-img-src") +
+					'" class="select-item-img-icon" />' +
+					state.text +
+					"</span>"
+			);
+		} else {
+			var $state = $("<span>" + state.text + "</span>");
+		}
 
+		return $state;
+	}
 
+	$(".js-simple-select2").each(function (index) {
+		var placeholder = $(this).attr("data-placeholder");
+		if (typeof placeholder !== "undefined" && placeholder.length > 0) {
+			$(this).addClass("simple-select2--placeholder-selected");
+		}
+		var select = $(this);
 
+		$(this)
+			.select2({
+				language: "ru",
+				theme: "custom-theme",
+				minimumResultsForSearch: Infinity,
+				width: '100%',
+				dropdownAutoWidth: true,
+				//width: "auto",
+				templateResult: sel2WithIconsTheme,
+				templateSelection: sel2WithIconsTheme,
+				dropdownParent: $(this).siblings(".simple-select2-items-wrapper")
+			})
+			.on("select2:open", function (e) {
+				$(this)
+					.siblings(".simple-select2-items-wrapper")
+					.addClass("simple-select2-items-wrapper--show");
+			})
+			.on("select2:closing", function (e) {
+				if ($(this).attr("data-close-anvaliable") !== "1") {
+					e.preventDefault();
+					select.attr("data-close-anvaliable", "1");
+					select
+						.siblings(".simple-select2-items-wrapper")
+						.removeClass("simple-select2-items-wrapper--show");
+					setTimeout(function () {
+						select.select2("close");
+					}, 350);
+				} else {
+					$(this).attr("data-close-anvaliable", "2");
+				}
+				//$(this).select2('close');
+			})
+			.on("select2:select", function (e) {
+				$(this).removeClass("simple-select2--placeholder-selected");
+			});
+	});
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 //libs
 @@include("./libs/jquery-3.7.0.min.js")
 @@include("./libs/swiper-bundle.min.js")
+@@include("./libs/select2.full.min.js")
 
 //my files
 @@include("./import/main.js")
