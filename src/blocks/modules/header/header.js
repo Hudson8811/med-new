96ffd3efb2574b5c40396463666c05ada079c1dd
PipZip <37911683+PipZip@users.noreply.github.com>
2024-02-22 12:56:50 +0600

    header mobile menu

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -7,7 +7,14 @@ $(document).ready(function (){
 		$(".header-mob-menu").addClass("_active");
 	});
 
+    $(".js-close-main-menu").on("click", function (){
+		event.preventDefault();
+		$(".header-mob-menu").removeClass("_active");
+		$(".menu-overlay").removeClass("_active").fadeOut(300);
+	});
+
 	$(".menu-overlay").on("click", function (){
+		event.preventDefault();
 		$(".header-mob-menu").removeClass("_active");
 		$(".menu-overlay").removeClass("_active").fadeOut(300);
 	});
